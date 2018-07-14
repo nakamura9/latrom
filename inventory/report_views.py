bc3fbfc932e0fb8fc1581909619905f5cec42f80
caleb kandoro <kandoroc@hotmail.com>
2018-07-14 21:20:32 +0200

    added inventory reports

---
+++ inventory/report_views.py
@@ -0,0 +1,28 @@
+import datetime
+import os 
+
+from django.views.generic import TemplateView
+
+import models 
+from common_data.utilities import load_config
+
+class InventoryReport(TemplateView):
+    template_name = os.path.join('inventory', 'reports', 'inventory_report.html')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(InventoryReport, self).get_context_data(*args, **kwargs)
+        context['items'] = models.WareHouseItem.objects.all()
+        context['date'] = datetime.date.today()
+        context.update(load_config())
+        return context
+
+
+class OutstandingOrderReport(TemplateView):
+    template_name = os.path.join('inventory', 'reports', 'outstanding_orders.html')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(OutstandingOrderReport, self).get_context_data(*args, **kwargs)
+        context['orders'] = models.Order.objects.all()
+        context['date'] = datetime.date.today()
+        context.update(load_config())
+        return context
